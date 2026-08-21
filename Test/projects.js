cb1c9b835989093acf9a936fa5ccb2f6d5f7faee
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-08-21 15:33:08 +0100

    Create projects.js

---
+++ Test/projects.js
@@ -0,0 +1,5 @@
+const projects = [
+{ "name": "Project 1", "language": "C++", "description": "A project", "duration": "6", "date": "01-01-26", "size": "small" },
+{ "name": "Project 2", "language": "Python", "description": "A project", "duration": "6", "date": "01-04-26", "size": "medium" },
+{ "name": "Project 3", "language": "Fortran", "description": "A special project", "duration": "5", "date": "01-04-26", "size": "large" },
+];
